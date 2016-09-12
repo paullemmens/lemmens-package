47683b62900c080a5bbdc38f45d6bc14328f44a0
Paul Lemmens <paul.lemmens@gmail.com>
2016-09-12 21:59:00 +0200

    initial commit of ggplot (theming) code

---
+++ R/ggplot2.R
@@ -0,0 +1,55 @@
+#' @title Collection of functions and settings for ggplot2
+#'
+#' @description
+#' Over the years, I developed a personalized style/theme for use in ggplot2. In
+#' earlier versions, this theme, and some other personalizations were in my
+#' .Rprofile that I synced across machines via Tresorit.
+#'
+#' @details
+#' To make things a little clearer and more consistent with other (packages), almost
+#' everything has been renamed. \emph{This will most definately break older scripts.}
+#'
+#' \code{theme_lemmens} is my old theme2 that is a cousin from mytheme and
+#' distinguishes itself by not using boxes and other decorating stuff around
+#' ggplot2's facets. \code{theme_l} is an abbreviation.
+#'
+#' @export
+theme_lemmens <- theme_bw() +
+	theme(text=element_text(size=18), 
+        axis.title.x=element_text(vjust=-0.2), 
+	      axis.title.y=element_text(angle=90, vjust=1),
+        panel.border=element_rect(colour='black', size=1.2),
+        strip.background = element_rect(colour = NA, fill = NA),
+#        strip.text = element_text(size = rel(1.0)),
+        legend.position='bottom')
+#' @export
+theme_l <- theme_lemmens
+
+
+pd <- position_dodge(width=0.7)
+
+## Different geom defaults that I predominantly used in my pre-Cairo days.
+mygeoms <- function(use=TRUE, reset=!use) {
+	if (use) {
+		update_geom_defaults('point', list(size=3.5))
+		update_geom_defaults('errorbar', list(size=1.0, width=0.3))
+		update_geom_defaults('line', list(size=1.5))
+		update_geom_defaults('boxplot', list(size=0.8))
+		update_geom_defaults('smooth', list(size=1.0))
+	} else {
+		update_geom_defaults('point', list(size=2))
+		update_geom_defaults('errorbar', list(size=0.5, width=0.5))
+		update_geom_defaults('line', list(size=0.5))
+		update_geom_defaults('boxplot', list(size=0.5))
+		update_geom_defaults('smooth', list(size=0.5))
+	}
+}
+
+## Lastly theme adaptation that I regularly use.
+theme_lemmens.old <- theme_bw() +
+	theme(text=element_text(size=18), 
+        axis.title.x=element_text(vjust=-0.2), 
+	      axis.title.y=element_text(angle=90, vjust=1),
+        panel.border=element_rect(colour='black', size=1.2),
+        legend.position='bottom')
+
